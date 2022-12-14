31578251f3d63377cee0b4287e97df01977c01cb
androidblog <androidblog@yandex.ru>
2022-12-14 12:31:25 +0400

    Change taskListSlice using redux toolkit

--- src/features/TaskList/taskListSlice.js
+++ src/features/TaskList/taskListSlice.js
@@ -1,40 +1,62 @@
 import { Action } from "@remix-run/router";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const TaskList = () => {
     const allTasks = useSelector()
 }
 
-const addTodo = (task) => {
-    return {
-        type: 'taskList/addTask',
-        payload: todo
-    }
-}
-
-const toggleTodo = (task) => {
-    return {
-        type: 'taskList/toggleTask',
-        payload: task
-    }
-}
-
-const taskList = (state = [], actiion) => {
-    switch (action.type) {
-        case 'taskList/addTask':
-            return [
-                ...state,
-                {
-                    id: action.payload.id,
-                    text: action.payload.text,
-                    completed: false
-                }
-            ];
-        case 'taskList/toggleTask':
+const options = {
+    name: 'taskList',
+    initialState: [],
+    reducers: {
+        addTask: (state, action) => {
+            return [...state, 
+            {
+                id: action.payload.id,
+                text: action.payload.text,
+                completed: false
+            }]
+        },
+        toggleTask: (state, action) => {
             return state.map(task => 
                 task.id === action.payload.id ? {...task, completed: !task.completed} : task);
-        default:
-            return state;
+        }
+
     }
 }
+export const taskListSlice = createSlice(options);
+
+// const addTask = (task) => {
+//     return {
+//         type: 'taskList/addTask',
+//         payload: todo
+//     }
+// }
+
+// const toggleTask = (task) => {
+//     return {
+//         type: 'taskList/toggleTask',
+//         payload: task
+//     }
+// }
+
+// const taskList = (state = [], actiion) => {
+//     switch (action.type) {
+//         case 'taskList/addTask':
+//             return [
+//                 ...state,
+//                 {
+//                     id: action.payload.id,
+//                     text: action.payload.text,
+//                     completed: false
+//                 }
+//             ];
+//         case 'taskList/toggleTask':
+//             return state.map(task => 
+//                 task.id === action.payload.id ? {...task, completed: !task.completed} : task);
+//         default:
+//             return state;
+//     }
+// }
